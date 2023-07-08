0f77913d7a517af6bb66bb9ddd9b5613b7804afa
preetsimon <77456490+preetsimon@users.noreply.github.com>
2023-07-08 10:22:04 -0700

    template displayed list

--- src/features/product-list/productListSlice.js
+++ src/features/product-list/productListSlice.js
@@ -1,5 +1,5 @@
 import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
-import { fetchCount } from './productlistAPI';
+import { fetchCount } from './productListAPI';
 
 const initialState = {
   value: 0,
