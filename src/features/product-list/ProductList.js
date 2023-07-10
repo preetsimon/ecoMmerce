5cdb3eb3d749c070d89eb42bb344b88c4a8e0a94
preetsimon <77456490+preetsimon@users.noreply.github.com>
2023-07-09 18:19:30 -0700

    login, signup routing

--- src/features/product-list/ProductList.js
+++ src/features/product-list/ProductList.js
@@ -435,7 +435,7 @@ export default function ProductList() {
                   <a
                     href="#"
                     aria-current="page"
-                    className="relative z-10 inline-flex items-center bg-indigo-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="relative z-10 inline-flex items-center bg-orange-600 px-4 py-2 text-sm font-semibold text-white focus:z-20 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                   >
                     1
                   </a>
